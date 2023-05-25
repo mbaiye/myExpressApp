48f8da7c1a426591d30591ecf8b7c50f6e535bce
Moyosore Baiye <moyosorebaiye@gmail.com>
2023-05-25 23:15:28 +0100

    test run cicd

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Team-DevOps Loves to Break Stuff and Cook Beans' });
+  res.render('index', { title: 'Team-DevOps Workout May2023' });
 });
 
 module.exports = router;
